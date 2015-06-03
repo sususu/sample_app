1f025bdb222c6c2fd62a61bea787e447a65cb01b
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-06-03 11:32:23 +0900

    Contactページのテストを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,6 +45,11 @@ describe "StaticPages" do
   end
 
   describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       expect(page).to have_title("#{base_title} | Contact")
