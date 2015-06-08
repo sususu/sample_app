853cad1e43a675513baef55ad783f2ebf68648b4
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-06-08 17:45:58 +0900

    test: ユーザindexページのテストを追加

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -59,6 +59,11 @@ describe "AuthenticationPages" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "when attempting to visit a protected page" do
