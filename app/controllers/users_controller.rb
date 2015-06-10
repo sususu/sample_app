d288ef9b6aa4a929dcb78e6e2e012f3775a4f9cb
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-06-10 12:49:47 +0900

    feat: サインイン済みのユーザがUsers#new, Users#createアクションにアクセスしようとした場合，ルートURLにリダイレクトするようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :created_signed_in_user, only: [:new, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -51,6 +52,10 @@ class UsersController < ApplicationController
     end
 
     # Before actions
+
+    def created_signed_in_user
+      redirect_to root_url if signed_in?
+    end
 
     def signed_in_user
       unless signed_in?
