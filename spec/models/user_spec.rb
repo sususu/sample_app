6b7bebe2e52a8cdbac7477df5247ba8e33d069d4
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-06-10 16:18:06 +0900

    test: マイクロポストのフィードに対するテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -147,6 +148,16 @@ describe User do
       microposts.each do |micropost|
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
+    end
+
+    describe 'status' do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:user) { should include(newer_micropost) }
+      its(:user) { should include(older_micropost) }
+      its(:user) { should_not include(unfollowed_post) }
     end
   end
 end
